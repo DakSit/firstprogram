0fd524dd33799470e60ee1d6270419a4aa6d0c69
Dakota Sitton <dakotasitton@gmail.com>
2018-01-23 10:31:40 -0600

    First program Change

---
+++ helloworld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+}
